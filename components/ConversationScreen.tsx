a4e034f38723fdb3169380d7bb193f1e63d176ba
Vinh Thuy <rickhoang42@gmail.com>
2022-07-22 18:55:53 +0700

    feat: complete send function
    - scroll to bottom when send or receive messages

--- components/ConversationScreen.tsx
+++ components/ConversationScreen.tsx
@@ -1,20 +1,28 @@
-import { IconButton } from "@mui/material";
-import styled from "styled-components";
-import { useRecipient } from "../hooks/useRecipient";
-import { Conversation, IMessage } from "../types";
+import { IconButton } from '@mui/material';
+import styled from 'styled-components';
+import { useRecipient } from '../hooks/useRecipient';
+import { Conversation, IMessage } from '../types';
 import {
   convertFirestoreTimestampToDate,
   generateQueryGetMessages,
   transformMessage,
-} from "../utils/getMessagesInConversation";
-import RecipientAvatar from "./RecipientAvatar";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { useRouter } from "next/router";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../config/firebase";
-import { useCollection } from "react-firebase-hooks/firestore";
-import Message from "./Message";
+} from '../utils/getMessagesInConversation';
+import RecipientAvatar from './RecipientAvatar';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../config/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import Message from './Message';
+import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import SpokeIcon from '@mui/icons-material/Spoke';
+import GifBoxIcon from '@mui/icons-material/GifBox';
+import SendIcon from '@mui/icons-material/Send';
+import { ChangeEventHandler, KeyboardEventHandler, MouseEventHandler, useLayoutEffect, useRef, useState } from 'react';
+import { addDoc, collection, doc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 const StyledRecipientHeader = styled.div`
   position: sticky;
@@ -52,18 +60,44 @@ const StyledHeaderIcon = styled.div`
 `;
 
 const StyledMessageContainer = styled.div`
-  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  justify-content: flex-end;
+  padding: 10px 20px 10px 10px;
   background-color: #ffffff;
   min-height: 90vh;
 `;
 
-const ConversationScreen = ({
-  conversation,
-  messages,
-}: {
-  conversation: Conversation;
-  messages: IMessage[];
-}) => {
+const StyledInputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  position: sticky;
+  bottom: 0;
+  background-color: white;
+  z-index: 100;
+  gap: 5px;
+  border-top: 1px solid #e0e0e0;
+  box-shadow: 0 -1px 3px rgba(0, 0, 0, 0.1);
+`;
+
+const StyledInput = styled.input`
+  flex-grow: 1;
+  background-color: #f3f3f5;
+  border-radius: 18px;
+  padding: 10px 15px;
+  outline: none;
+  border: none;
+`;
+
+const EndOfMessagesForAutoScroll = styled.div`
+  margin-bottom: 10px;
+`;
+
+const ConversationScreen = ({ conversation, messages }: { conversation: Conversation; messages: IMessage[] }) => {
+  const [inputValue, setInputValue] = useState('');
+
   const [loggedInUser, _loading, _error] = useAuthState(auth);
 
   const conversationUsers = conversation.users;
@@ -76,19 +110,13 @@ const ConversationScreen = ({
 
   const queryGetMessages = generateQueryGetMessages(conversationId as string);
 
-  const [messagesSnapshot, messagesLoading, __error] =
-    useCollection(queryGetMessages);
+  const [messagesSnapshot, messagesLoading, __error] = useCollection(queryGetMessages);
 
   const showMessages = () => {
     // If frontend is loading messages behind the scene, show messages from Next SSR (passed down from [id].tsx)
     if (messagesLoading) {
       return messages.map((message) => (
-        <Message
-          key={message.id}
-          message={message}
-          recipient={recipient}
-          recipientEmail={recipientEmail}
-        />
+        <Message key={message.id} message={message} recipient={recipient} recipientEmail={recipientEmail} />
       ));
     }
     // if messagesSnapshot are loaded, show messagesSnapshot instead of messages from Next SSR
@@ -106,20 +134,71 @@ const ConversationScreen = ({
     return null;
   };
 
+  const handleOnSubmit: ChangeEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInputOnChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const handleInputOnKeyPress: KeyboardEventHandler<HTMLInputElement> = async ({ key }) => {
+    if (key === 'Enter') {
+      if (!inputValue) return;
+      await addMessageToDbAndUpdateLastSeen();
+    }
+  };
+
+  console.log(loggedInUser?.email);
+
+  const addMessageToDbAndUpdateLastSeen = async () => {
+    // update your last seen when you send a message
+    await setDoc(
+      doc(db, 'users', loggedInUser?.email as string),
+      {
+        lastSeen: serverTimestamp(),
+      },
+      {
+        merge: true,
+      }
+    );
+
+    // add message to db
+    await addDoc(collection(db, 'messages'), {
+      conversation_id: conversationId,
+      sent_at: serverTimestamp(),
+      text: inputValue,
+      user: loggedInUser?.email,
+    });
+
+    // reset input fields
+    setInputValue('');
+  };
+
+  const handleMessageOnClick: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    if (!inputValue) return;
+    await addMessageToDbAndUpdateLastSeen();
+  };
+
+  const endOfMessagesRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    if (endOfMessagesRef.current) {
+      endOfMessagesRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  useLayoutEffect(() => {
+    scrollToBottom();
+  }, [messagesSnapshot]);
   return (
     <>
       <StyledRecipientHeader>
-        <RecipientAvatar
-          recipient={recipient}
-          recipientEmail={recipientEmail}
-        />
+        <RecipientAvatar recipient={recipient} recipientEmail={recipientEmail} />
         <StyledHeaderInfo>
           <StyledH3>{recipientEmail}</StyledH3>
-          {recipient && (
-            <span>
-              Last active: {convertFirestoreTimestampToDate(recipient.lastSeen)}
-            </span>
-          )}
+          {recipient && <span>Last active: {convertFirestoreTimestampToDate(recipient.lastSeen)}</span>}
         </StyledHeaderInfo>
         <StyledHeaderIcon>
           <IconButton>
@@ -130,7 +209,38 @@ const ConversationScreen = ({
           </IconButton>
         </StyledHeaderIcon>
       </StyledRecipientHeader>
-      <StyledMessageContainer>{showMessages()}</StyledMessageContainer>
+      <StyledMessageContainer>
+        {showMessages()}
+        {/* for auto scroll to bottom when new message is added*/}
+        <EndOfMessagesForAutoScroll ref={endOfMessagesRef} />
+      </StyledMessageContainer>
+
+      <StyledInputContainer onSubmit={handleOnSubmit}>
+        <IconButton>
+          <AddCircleOutlinedIcon sx={{ color: '#0084ff' }} />
+        </IconButton>
+        <IconButton sx={{ color: '#0084ff' }}>
+          <InsertPhotoIcon />
+        </IconButton>
+        <IconButton sx={{ color: '#0084ff' }}>
+          <SpokeIcon />
+        </IconButton>
+        <IconButton sx={{ color: '#0084ff' }}>
+          <GifBoxIcon />
+        </IconButton>
+        <StyledInput
+          placeholder="Aa"
+          value={inputValue}
+          onChange={handleInputOnChange}
+          onKeyDown={handleInputOnKeyPress}
+        />
+        <IconButton sx={{ color: '#0084ff' }}>
+          <EmojiEmotionsIcon />
+        </IconButton>
+        <IconButton onClick={handleMessageOnClick} disabled={!inputValue} sx={{ color: '#0084ff' }}>
+          <SendIcon />
+        </IconButton>
+      </StyledInputContainer>
     </>
   );
 };
--- config/firebase.ts
+++ config/firebase.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDM1AiP1Fx_U9Vc27sfWDDNoEGtlhb5YxQ",
-  authDomain: "next-chat-app-eed60.firebaseapp.com",
-  projectId: "next-chat-app-eed60",
-  storageBucket: "next-chat-app-eed60.appspot.com",
-  messagingSenderId: "915765065610",
-  appId: "1:915765065610:web:5985e8ff4b30a162337a73",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
